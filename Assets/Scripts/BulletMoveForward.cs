adb9be21eae6853082ed644525237e113f18bcec
LachlanRichardsUSC <ljr044@student.usc.edu.au>
2024-03-30 07:28:24 +1000

    Implemented Interfaces for Moving Objects
    Implemented Interfaces for Enemy Object (falling rocks) and the player's laser projectile, as they share a very similar behaviour.
    
    Minor Tweaks: Indentation, Coding Conventions

--- Assets/Scripts/BulletMoveForward.cs
+++ Assets/Scripts/BulletMoveForward.cs
@@ -1,27 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletMoveForward : MonoBehaviour {
+public class BulletMoveForward : MonoBehaviour, IEngine
+{
 
     private float acceleration = 50f;
-
     private float initialVelocity = 5f;
-
     private Rigidbody2D ourRigidbody;
 
     // Use this for initialization
     void Start()
     {
         ourRigidbody = GetComponent<Rigidbody2D>();
-
         ourRigidbody.velocity = Vector2.up * initialVelocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move()
     {
         Vector2 ForceToAdd = Vector2.up * acceleration * Time.deltaTime;
-
         ourRigidbody.AddForce(ForceToAdd);
     }
+
 }
--- Assets/Scripts/EnemyMoveForward.cs
+++ Assets/Scripts/EnemyMoveForward.cs
@@ -1,27 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyMoveForward : MonoBehaviour {
+public class EnemyMoveForward : MonoBehaviour, IEngine
+{
 
     private float acceleration = 75f;
-
     private float initialVelocity = 2f;
-
     private Rigidbody2D ourRigidbody;
 
     // Use this for initialization
     void Start()
     {
         ourRigidbody = GetComponent<Rigidbody2D>();
-
         ourRigidbody.velocity = Vector2.down * initialVelocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move()
     {
         Vector2 ForceToAdd = Vector2.down * acceleration * Time.deltaTime;
-
         ourRigidbody.AddForce(ForceToAdd);
     }
 }
